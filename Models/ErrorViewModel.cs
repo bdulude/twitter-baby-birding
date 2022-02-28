f78ccd27190258ff876ab4c505462e062ebe1181
bdulude <bdulude@gmail.com>
2022-02-28 12:06:12 -0800

    dotnet init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace twitter_baby_birding.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
