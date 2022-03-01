3d6c24b898fc86edbd93dee3b8ab2b0c591c9d20
BLD\FriedRice <anthonyweido@gmail.com>
2022-02-28 20:38:55 -0800

    Twitter Sharp Keys Implementation

--- Startup.cs
+++ Startup.cs
@@ -7,14 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CoreTweet;
+using TwitterSharp;
 
 namespace twitter_baby_birding
 {
+    public static class TwitterKeys
+    {
+        public static string API{get;set;}
+        public static string Secret{get;set;}
+        public static string Bearer{get;set;}
+    }
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
@@ -23,6 +32,11 @@ namespace twitter_baby_birding
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            // OAuth.OAuthSession session = OAuth.Authorize(Configuration["Twitter:Key"], Configuration["Twitter:Secret"]);
+            // System.Diagnostics.Process.Start(session.AuthorizeUri.AbsoluteUri);
+            TwitterKeys.API = Configuration["Twitter:Key"];
+            TwitterKeys.Secret =  Configuration["Twitter:Secret"];
+            TwitterKeys.Bearer = Configuration["Twitter:Bearer"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
