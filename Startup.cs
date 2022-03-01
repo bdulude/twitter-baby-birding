21417bde1871ff999a80f9ae9dd0464bd369a7bb
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-01 15:48:18 -0800

    Removing 'using CoreTweet'

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CoreTweet;
 using TwitterSharp;
 
 namespace twitter_baby_birding
