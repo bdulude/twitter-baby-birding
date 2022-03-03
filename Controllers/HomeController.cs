d8924ff7b5523df84efe63a41bda7484999e72e7
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-03 15:07:48 -0800

    Added MultiBarf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -110,6 +110,89 @@ namespace twitter_baby_birding.Controllers
             return View("About");
         }
 
+              [HttpGet("MultiTweet")]
+        public IActionResult MultiTweet()
+        {
+            ViewBag.UsersCount = 2;
+            return View();    
+        }
+
+
+        [HttpPost("multiBarf")]
+        public async Task<IActionResult> GenerateMulti(TwitterHandle username)
+        {
+            //Prelimanary Check to see if any of the users are null
+            for (int i = 0; i < username.MultiHandle.Count; i++)
+            {
+                string user = username.MultiHandle[i];
+                if(user== null)
+                {
+                    ModelState.AddModelError($"MultiHandle[{i}]", "Error! There is an empty Field!");
+                    return View("MultiTweet");
+                }
+                else if (user.Length > 15)
+                {
+                    ModelState.AddModelError($"MultiHandle[{i}]", $"Error! {user} is too long! Less than 15 Characters");
+                    return View("MultiTweet");
+                }
+            }
+            //Setting the variables to be more readable
+            int NumberOfUsers = username.MultiHandle.Count;
+            List<string> UserHandles = username.MultiHandle;
+            TwitterSharp.Response.RUser.User[] UsersArray= new TwitterSharp.Response.RUser.User[NumberOfUsers];
+            
+            for (int i = 0; i < username.MultiHandle.Count; i++)
+            {
+                //Set the User
+                UsersArray[i] = await TweetFetcher.GetUser(username.MultiHandle[i]);
+                if(UsersArray[i] == null)
+                {
+                    //If the User is null add an error to the Model
+                    ModelState.AddModelError($"MultiHandle[{i}]", "Error! Handle is not valid!!");
+                }
+            }
+            //If Any of the Users are null Then the state is invalid
+            if(!ModelState.IsValid)
+            {
+                    ViewBag.UsersCount = UserHandles.Count;
+                    ViewBag.UsersHandles = UserHandles;
+                    return View("MultiTweet",username);
+            }
+
+            // Determine how much data per user
+            // int tweetsPerUser = (int)MathF.Floor(100/NumberOfUsers);
+            int tweetsPerUser = 100;
+
+            //Create an array to hold all the gotten tweets
+            TwitterSharp.Response.RTweet.Tweet[] TweetArr = new TwitterSharp.Response.RTweet.Tweet[0];
+            foreach(TwitterSharp.Response.RUser.User User in UsersArray)
+            {
+                //Get Tweets for that user and concat them onto the newTweets
+                TwitterSharp.Response.RTweet.Tweet[] newTweets = await TweetFetcher.FindByHandle(User,tweetsPerUser);
+                TweetArr = TweetArr.Concat(newTweets).ToArray();
+            }
+
+            string[] tweets = TweetArr.Where(t => !t.Text.StartsWith("RT @")).Select(t => t.Text).ToArray();
+
+            // Create a new model
+            var model = new StringMarkov(1);
+
+            // Train the model
+            model.Learn(tweets);
+
+            // Create some permutations
+            List<string> barf = new List<string>();
+            barf.Add(model.Walk().First());
+            barf.Add(HttpUtility.UrlEncode(barf[0]));
+
+            // string barf = model.Walk().First();
+
+            // Pass generated tweet to a ViewModel
+
+            return View("Generate", barf);
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
