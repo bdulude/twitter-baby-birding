249b48e228b466e0636f45ead184a02da5606236
BLD\FriedRice <anthonyweido@gmail.com>
2022-03-03 14:41:41 -0800

    Added A MultiHandle List for MultiTweet

--- Models/TwitterHandle.cs
+++ Models/TwitterHandle.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
+
 namespace twitter_baby_birding.Models
 {
     public class TwitterHandle
     {
         public string Handle { get; set; }
+
+        public List<string> MultiHandle{get;set;}
     }
 }
