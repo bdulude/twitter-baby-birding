23f8eed0e4c5e727fa3b6a58d5e1a522fe0c8f27
bduehlin <bduehlin@gmail.com>
2022-03-04 13:55:23 -0800

    trying to fix

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace twitter_baby_birding.Models
 {
+    [Keyless]
     public class User
     {
-        [Key]
         public string Name {get; set;}
         public List<Tweet> Tweets {get; set;}
     }
